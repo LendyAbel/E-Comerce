8643fb8ed05409559fdf6c6b5cac9dfc3bd2544b
LendyAbel <lendyabel93@gmail.com>
2025-09-18 13:24:19 +0200

    feat: Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import Header from './components/layout/Header'
+import { Router, Routes, Route } from "react-router";
+
+import Header from "./components/layout/Header";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
-  
-
   return (
-    <div className=''>
+    <div>
       <Header>
-        App
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
       </Header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Products = () => {
+  return (
+    <div>
+      PRODUCTS
+    </div>
+  )
+}
+
+export default Products
